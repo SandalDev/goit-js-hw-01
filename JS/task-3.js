72861d4709ca9a30cc73106be46f960de69280d1
Михайло Тарасенко <mishabos.bos@gmail.com>
2025-09-28 11:14:47 +0300

    fix: corrected getElementWidth calculation

--- JS/task-3.js
+++ JS/task-3.js
@@ -1,4 +1,12 @@
-function getElementWidth(content, padding, border) {}
+function getElementWidth(content, padding, border) {
+  const contentNum = parseFloat(content);
+  const paddingNum = parseFloat(padding);
+  const borderNum = parseFloat(border);
+
+  const width = contentNum + 2 * paddingNum + 2 * borderNum;
+
+  return width;
+}
 
 console.log(getElementWidth('50px', '8px', '4px')); // 74
 console.log(getElementWidth('60px', '12px', '8.5px')); // 101
